ac2b36b32c2ff3567bfa5c93bb83726c85b08d34
ygorthiago <ygorthiagoff@outlook.com>
2021-10-03 01:04:37 -0300

    feat: use stopPropagation to not trigger openDetails when button is clicked

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -12,25 +12,26 @@ export function BookCard({ book, openDetails }: IBookCard): JSX.Element {
   const { addBookToCart } = useFlipContext();
 
   return (
-    <section className={s.bookCardContainer}>
-      <div
-        className={s.book}
-        onClick={openDetails}
-        data-testid="book-card-info"
-        onKeyPress={openDetails}
-      >
-        <h3 className={s.bookTitle}>{book.title}</h3>
-        <img src={book.image} alt={book.title} loading="lazy" />
-        <div className={s.bookInfos}>
-          <legend>{book.subtitle}</legend>
-          <p>{book.price}</p>
-        </div>
+    <section
+      className={s.bookCardContainer}
+      onClick={openDetails}
+      data-testid="book-card-info"
+      onKeyPress={openDetails}
+    >
+      <h3 className={s.bookTitle}>{book.title}</h3>
+      <img src={book.image} alt={book.title} loading="lazy" />
+      <div className={s.bookInfos}>
+        <legend>{book.subtitle}</legend>
+        <p>{book.price}</p>
       </div>
       <button
         type="button"
         data-testid="add-book-button"
         className={s.addToCardButton}
-        onClick={() => addBookToCart(book)}
+        onClick={e => {
+          addBookToCart(book);
+          e.stopPropagation();
+        }}
       >
         <div>
           <MdAddShoppingCart />
