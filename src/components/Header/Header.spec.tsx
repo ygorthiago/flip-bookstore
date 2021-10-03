5f487db009b6bd5f386eee74625490da8d351e32
ygorthiago <ygorthiagoff@outlook.com>
2021-10-03 02:01:06 -0300

    test: checking text when cart have only 1 item

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import { ReactNode } from 'react';
 import { Header } from '.';
+import { useFlipContext } from '../../contexts/useFlipContext';
 
 jest.mock('react-router-dom', () => {
   return {
@@ -8,9 +9,13 @@ jest.mock('react-router-dom', () => {
   };
 });
 
-jest.mock('../../contexts/useFlipContext', () => {
-  return {
-    useFlipContext: () => ({
+const mockedFlipContext = useFlipContext as jest.Mock;
+
+jest.mock('../../contexts/useFlipContext');
+
+describe('Header Component', () => {
+  beforeEach(() => {
+    mockedFlipContext.mockReturnValue({
       cart: [
         {
           title: 'Java Notes for Professionals',
@@ -19,21 +24,21 @@ jest.mock('../../contexts/useFlipContext', () => {
           price: '$0.00',
           image: 'https://itbook.store/img/books/1001630936399.png',
           url: 'https://itbook.store/books/1001630936399',
+          amount: 1,
         },
         {
           title: 'Java Notes for Professionals',
           subtitle: 'Your Guided Tour Through the Programming Jungle',
-          isbn13: '1001630936399',
+          isbn13: '1001630936398',
           price: '$0.00',
           image: 'https://itbook.store/img/books/1001630936399.png',
           url: 'https://itbook.store/books/1001630936399',
+          amount: 2,
         },
       ],
-    }),
-  };
-});
+    });
+  });
 
-describe('Header Component', () => {
   it('should be able to render Header component', () => {
     const headerComponent = render(<Header />);
 
@@ -45,5 +50,25 @@ describe('Header Component', () => {
 
     const cartSizeCounter = getByTestId('cart-size');
     expect(cartSizeCounter.textContent).toBe('2 itens');
+  });
+
+  it(`should show 'item' when cart have only 1 item`, () => {
+    mockedFlipContext.mockReturnValue({
+      cart: [
+        {
+          title: 'Java Notes for Professionals',
+          subtitle: 'Your Guided Tour Through the Programming Jungle',
+          isbn13: '1001630936398',
+          price: '$0.00',
+          image: 'https://itbook.store/img/books/1001630936399.png',
+          url: 'https://itbook.store/books/1001630936399',
+          amount: 2,
+        },
+      ],
+    });
+    const { getByTestId } = render(<Header />);
+
+    const cartSizeCounter = getByTestId('cart-size');
+    expect(cartSizeCounter.textContent).toBe('1 item');
   });
 });
