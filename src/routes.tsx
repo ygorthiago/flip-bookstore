b98090f470e46d4fd234e8fb3ac8420658044a83
ygorthiago <ygorthiagoff@outlook.com>
2021-10-09 10:27:22 -0300

    feat: add PageNotFound to router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Cart } from './pages/Cart';
 import { Home } from './pages/Home';
+import { PageNotFound } from './pages/PageNotFound';
 
 export function Routes(): JSX.Element {
   return (
@@ -8,6 +9,7 @@ export function Routes(): JSX.Element {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/cart" exact component={Cart} />
+        <Route component={PageNotFound} />
       </Switch>
     </BrowserRouter>
   );
